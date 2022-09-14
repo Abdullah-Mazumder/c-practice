580bb90a117506a73be8eac30179ea05cc99beec
Abdullah <abdullahatik483@gmail.com>
2022-09-14 17:54:05 +0600

    4th day of learning c

---
+++ 3rdDay.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+const float PI = 3.1416;
+
+int main()
+{
+    // int num = 45;
+    // float num2 = 45.45f;
+    // char ch = 'A';
+
+    // printf("%d\n", num);
+    // printf("%f\n", num2);
+    // printf("%c\n", ch);
+
+    // num = num2;
+    // printf("%d\n", num);
+
+    // float a, b;
+    // scanf("%f %f", &a, &b);
+    // printf("%f %f", a, b);
+
+    // float num1, num2, result;
+    // printf("please enter two numbers: ");
+    // scanf("%f %f", &num1, &num2);
+    // result = num1 + num2;
+    // printf("%f + %f = %.2f\n", num1, num2, result);
+
+    // result = num1 - num2;
+    // printf("%f - %f = %.2f\n", num1, num2, result);
+
+    // result = num1 * num2;
+    // printf("%f * %f = %.2f\n", num1, num2, result);
+
+    // result = num1 / num2;
+    // printf("%f / %f = %.2f\n", num1, num2, result);
+
+    char n = '5';
+    printf("%c", n);
+
+    return 0;
+}
---
+++ 4thDay.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    /*
+    // Signed Integer data
+    printf("Signed Integers.....\n");
+
+    // short - %hi, %d
+    short a = 45;
+    printf("I am short - %d\n", a);
+
+    // Int - %d, %i
+    int b = 1000;
+    printf("I am int - %i\n", b);
+
+    // Long - %ld, %li
+    long c = 1200l;
+    printf("I am long - %ld\n", c);
+
+    // Long Long - %lld, %lli
+    long long d = 34345435ll;
+    printf("I am long long - %lld\n", d);
+
+    // Octal Int - %o
+    int o = 0777;
+    printf("I am octal - %o\n", o);
+    printf("I am decimal of octal - %d\n", o);
+
+    // Hexadecimal Int - %x, %X
+    int h = 0Xfff;
+    printf("I am hexadecimal - %X\n", h);
+    printf("I am decimal of hexadecimal - %d\n", h);
+
+    // Unsigned Integers
+    printf("Unsigned Integers.....\n");
+
+    // Unsigned short - %hu
+    unsigned short us = 41;
+    printf("I am unsigned short - %hu\n", us);
+
+    // Unsigned Int - %u
+    unsigned int ui = 4533;
+    printf("I am unsigned int - %u\n", ui);
+
+    // Unsigned long - %lu
+    unsigned long ul = 453345;
+    printf("I am unsigned long - %lu\n", ul);
+
+    // Unsigned long long - %llu
+    unsigned long ull = 453345454;
+    printf("I am unsigned long long - %llu\n", ull);
+
+    unsigned short negative = -2;
+    printf("Negative Unsigned - %hu\n", negative);
+    */
+
+    /*
+     // Float - %f
+     float f = 489.123f;
+     printf("Float - %f\n", f);
+
+     // Double - %lf
+     double d = 453454.454;
+     printf("Double - %lf\n", d);
+
+     // Long Double - %Lf
+     long double ld = 4354354545.454;
+     printf("Long Double - %Lf\n", ld);
+    */
+
+    /*
+    // Char - %c
+    char a = 65;
+    printf("Char - %c\n", a);
+    char b = 'B';
+    printf("Char - %d\n", b);
+    */
+
+    /*
+     char name[20];
+     name[0] = 'A';
+     name[1] = 'b';
+     name[2] = 'd';
+     name[3] = 'u';
+     name[4] = 'l';
+     printf("%s\n", name);
+
+     char name1[20] = {'A', 'b', 'd', 'u', 'l', 'l',
+                       'a', 'h'};
+     printf("%s\n", name1);
+
+     char name2[20] = "Abdullah Mazumder";
+     printf("%s\n", name2);
+
+     char myName[30];
+     printf("What is your name?");
+     scanf("%s", &myName);
+     printf("Thank you, %s\n", myName);
+     */
+
+    /*
+    bool is_logged_in = false;
+
+    if (is_logged_in)
+    {
+        printf("User Logged In\n");
+    }
+    else
+    {
+        printf("User Logged Out\n");
+    }
+    */
+
+    /*
+     char ch = 'T';
+     int r = (int)ch + 100;
+     printf("%c %d %X\n", ch, ch, ch);
+     printf("Result: %d\n", r);
+
+     float f = (float)r;
+     printf("Float - %f\n", f);
+
+     float a = 4.5f;
+     float b = 5.3f;
+     float c = 6.2f;
+
+     int result = (int)a + (int)b + (int)c;
+     printf("Total (conversiion): %d\n", result);
+     */
+
+    return 0;
+}
+
+/*
+ 2 = 0000000000000010
+   = 1111111111111101 1's Complement
+                   +1
+     1111111111111110
+*/
