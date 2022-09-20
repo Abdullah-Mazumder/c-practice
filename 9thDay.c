1399d7604bbead3bcadf4bb7f9c624ee14bde364
Abdullah <abdullahatik483@gmail.com>
2022-09-20 18:04:05 +0600

    9th day of learning c

---
+++ 9thDay.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/*
+
+int fibonacchi(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacchi(n - 1) + fibonacchi(n - 2);
+    }
+}
+
+int main()
+{
+    for (int i = 0; i <= 42; i++)
+    {
+        printf("%d\t", fibonacchi(i));
+    }
+
+    return 0;
+}
+
+*/
+
+int main()
+{
+    /*
+    int i = 76;
+    int *b = &i;
+    int *c = NULL;
+
+    printf("%d", c);
+    */
+
+    /*
+     char a = '3';
+     char *ptra = &a;
+     printf("%d\n", ptra);
+     printf("%d\n", ptra - 1);
+     */
+
+    int arr[10] = {23, 234, 34};
+    int *ptr = arr;
+    printf("%d\n", ptr);
+    printf("%d\n", &arr[0]);
+    printf("%d\n", arr + 1);
+
+    return 0;
+}
